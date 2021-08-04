275ac782c2dc581f54ef0233d58e042c3668976b
mizuki-n-2 <mizukizhangze@gmail.com>
2021-08-05 07:46:59 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root "tasks#index"
 
-  resource :tasks
-
+  get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+  get "/tasks/:id/edit", to: "tasks#edit"
+  post "/tasks", to: "tasks#create"
+  patch "/tasks/:id", to: "tasks#update"
+  delete "/tasks/:id", to: "tasks#destroy"
 end
