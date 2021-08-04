c75f774d7ce17ead37fb5a05f5f61ade141f5953
mizuki-n-2 <mizukizhangze@gmail.com>
2021-08-05 08:42:43 +0900

    add crud process

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,34 @@
 class TasksController < ApplicationController
   def index
-    
+    @tasks = Task.all
+    render "tasks/index"
   end
 
   def new
-  
+    render "tasks/new"
   end
 
   def create
-
+    title = params[:title]
+    @task = Task.create(title: title)
+    redirect_to("/", status: :created)
   end
 
   def edit
-
+    @task = Task.find(params[:id])
+    render "tasks/edit"
   end
 
   def update
-
+    title = params[:title]
+    task = Task.find(params[:id])
+    task.update(title: title)
+    redirect_to("/", status: :ok)
   end
 
   def destroy
-
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to("/", status: 204)
   end
 end
