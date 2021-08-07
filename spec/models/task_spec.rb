26699942732d6b465dfed4ab22d3a85ffde563d4
mizuki-n-2 <mizukizhangze@gmail.com>
2021-08-08 03:41:39 +0900

    introduce factorybot

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  let(:task) { create(:task) }
+
   describe 'Validation' do
     context 'titleを設定したときに作成できる' do
-      task = Task.create(
-        title: 'やること'
-      )
       it { expect(task).to be_valid }
     end
+
     context 'titleが0文字のとき作成できない' do
-      task = Task.create(
+      task_title_blank = Task.create(
         title: ''
       )
-      it { expect(task).to be_invalid }
+      it { expect(task_title_blank).to be_invalid }
     end
   end
 end
